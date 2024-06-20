64a3ea11d95d9aaa9ecdf67e19f5562b10a86f39
Ignacio El Kadre <imefisto@gmail.com>
2024-06-20 11:15:11 -0300

    feat: added test for success sign up

---
+++ tests/UseCases/SignUpInteractorTest.php
@@ -0,0 +1,56 @@
+<?php
+namespace Imefisto\AuthLib\Testing\UseCases;
+
+use Imefisto\AuthLib\Domain\UserId;
+use Imefisto\AuthLib\Domain\UserRepository;
+use Imefisto\AuthLib\UseCases\SignUp\SignUpInteractor;
+use Imefisto\AuthLib\UseCases\SignUp\SignUpInputPort;
+use Imefisto\AuthLib\UseCases\SignUp\SignUpOutputPort;
+use Imefisto\AuthLib\UseCases\SignUp\SignUpRequest;
+use Imefisto\AuthLib\UseCases\SignUp\SignUpResponse;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+
+#[CoversClass(SignUpInteractor::class)]
+class SignUpInteractorTest extends TestCase
+{
+    private MockObject $userRepository;
+    private MockObject $output;
+    private MockObject $validator;
+    private SignUpInteractor $interactor;
+
+    protected function setUp(): void
+    {
+        $this->userRepository = $this->createMock(UserRepository::class);
+        $this->output = $this->createMock(SignUpOutputPort::class);
+        $this->interactor = new SignUpInteractor($this->userRepository, $this->output);
+    }
+
+    public function testSignUpWithValidData(): void
+    {
+        $username = 'user@example.com';
+        $password = 'some-password';
+        $id = 'some-user-id';
+
+        $request = new SignUpRequest($username, $password);
+
+        $this->userRepository->expects($this->once())
+                             ->method('existsByUsername')
+                             ->with($username)
+                             ->willReturn(false);
+
+        $this->userRepository->expects($this->once())
+                             ->method('createUser')
+                             ->with($username, $password)
+                             ->willReturn(new UserId($id));
+
+        $this->output->expects($this->once())
+                     ->method('userSignedUp')
+                     ->with($this->callback(function (SignUpResponse $response) use ($id) {
+                         return (string) $response->userId === $id;
+                     }));
+
+        $this->interactor->signUp($request);
+    }
+}
